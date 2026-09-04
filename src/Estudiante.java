ef24e535fb1b660998188bfecea9291945cad908
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-09-03 21:28:37 -0400

    JAVA-005 - Agregar mostrarInformacion

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -33,4 +33,9 @@ public class Estudiante {
     public void setEdad(int edad) {
         this.edad = edad;
     }
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Carrera: " + carrera);
+        System.out.println("Edad: " + edad);
+    }
 }

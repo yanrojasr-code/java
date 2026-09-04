c7a68c3248b830ab81892d5ee365a677cd3d43e0
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-09-03 21:26:08 -0400

    JAVA-004 - Agregar getters y setters

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -10,4 +10,27 @@ public class Estudiante {
 
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 }

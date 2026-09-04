55e92fe1475547aee67aa720ed8f51a7818fa1e2
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-09-03 22:10:06 -0400

    JAVA-011 - Agregar menu con while

--- src/Main.java
+++ src/Main.java
@@ -28,5 +28,25 @@ public class Main {
         for (int i=1; i <= 5; i++){
         System.out.println("Procesando estudiante" + i);}
 
+        int opcion = -1;
+        while (opcion != 0){
+
+            System.out.println("=== SISTEMA DUOC ===");
+            System.out.println("1. Mostrar estado");
+            System.out.println("2. Procesar operación");
+            System.out.println("0. Salir");
+
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
+
+            if (opcion == 1) {
+                System.out.println("Sistema operativo.");
+            } else if (opcion == 2) {
+                System.out.println("Procesando operación...");
+            } else if (opcion == 0) {
+                System.out.println("Cerrando sistema...");
+            }
+        }
+
     }
 }

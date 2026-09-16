20000325785c138ccc8a2285a4d4eb676bb1c97a
yanara <yan.rojasr@duocuc.cl>
2026-09-16 00:24:23 -0300

    final

--- Dispositivo/src/Main.java
+++ Dispositivo/src/Main.java
@@ -17,5 +17,17 @@ public  class Main {
         proyector.ejecutarDiagnostico();
         computador.ejecutarDiagnostico();
         impresora.ejecutarDiagnostico();
+
+        Dispositivo dispositivo1 = new Proyector("pryector sala 201", true);
+        Dispositivo dispositivo2 = new Computador("PC Laboratorio", true);
+        Dispositivo dispositivo3 = new Impresora("Impresora Biblioteca", true);
+
+        dispositivo1.ejecutarDiagnostico();
+        dispositivo2.ejecutarDiagnostico();
+        dispositivo3.ejecutarDiagnostico();
+
+        dispositivo1.mostrarEstado();
+        dispositivo2.mostrarEstado();
+        dispositivo3.mostrarEstado();
     }
 }

eb6ef16070a4feeaddae7acc6326e6fa066f696f
yanara <yan.rojasr@duocuc.cl>
2026-09-16 00:01:56 -0300

    Dispositivo

--- Dispositivo/src/Dispositivo.java
+++ Dispositivo/src/Dispositivo.java
@@ -3,4 +3,18 @@ public class Dispositivo {
     private String nombre;
     private  boolean activo;
 
+    public Dispositivo(String nombre, boolean activo) {
+        this.nombre = nombre;
+        this.activo = activo;
+    }
+    public  void mostrarEstado()
+    {
+        System.out.println(nombre + "-" + activo);
+    }
+    public  void  ejecutarDiagnostico()
+    {
+        System.out.println("Ejecutando diagnostico general....");
+    }
+
+
 }

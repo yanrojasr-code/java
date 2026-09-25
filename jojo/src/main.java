706ad21bae67706d0a39953eda00e7f8d0cab91a
yanara <yan.rojasr@duocuc.cl>
2026-09-25 20:59:38 -0300

    tercer ejercicio

--- jojo/src/main.java
+++ jojo/src/main.java
@@ -6,15 +6,26 @@ public class  main {
 public static  void maind (String[] args) {
     Scanner scanner = new Scanner(System.in);
 
+
     try {
         System.out.println("ingrese su edad");
         int edad = scanner.nextInt();
+        System.out.println("edad registrada " + edad);
 
-        System.out.println("edad registrada " + edad);}
+
+        System.out.println(" ingrese su nombre ");
+        String nombre = scanner.nextLine();
+
+        if (edad >= 18) {
+            System.out.println("acceso autorizado");
+        } else {
+            System.out.println("acceso restringido");
+        }
+    }
 
        catch(InputMismatchException e){
             System.out.println("Error: debes ingresar un numero. ");
-        }
+    }
 
 
   }

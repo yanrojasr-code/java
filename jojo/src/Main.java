2679f89a0d14bef619301427df4f44b0933d0cd5
yanara <yan.rojasr@duocuc.cl>
2026-09-25 20:23:01 -0300

    ejercicio 3

---
+++ jojo/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+public void main() {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println( "ingrese su edad: ");
+    int edad = scanner.nextInt();
+
+    System.out.println( "la edad registrada es: " + edad);
+}

4b5ab134af19f580737d928e7a0e4dc615d6285a
yanara <yan.rojasr@duocuc.cl>
2026-09-25 21:15:49 -0300

    resuelto

--- jojo/src/main.java
+++ jojo/src/main.java
@@ -1,3 +1,22 @@
     void main () {
 
+
+    double descuento = 0.10;
+    double total = 50000;
+
+    //la edad utilizada para evaluar una regla del sistema.
+
+        int edad = 20;
+
+     /*
+     * el calculo aplica el decuento definido
+     * antes de obtener el  total final.
+      */
+        double totalFinal = total - (total * descuento);
+
+
+
+
+
+
 }

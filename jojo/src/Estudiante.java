9e9bb05573281a507b5f14f304ed1bdb60246559
yanara <yan.rojasr@duocuc.cl>
2026-09-25 21:26:27 -0300

    ahora si

--- jojo/src/Estudiante.java
+++ jojo/src/Estudiante.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
+/**
+ * representa un estudiante en el sistema academico.
+ *
+ */
 public class Estudiante {
+
+    /**
+     * Determina si el estudiante aprueba segun su nota final
+     *
+     * @param notaFinal nota obtenida por el estudiante
+     * @return true su la nota es igual o superior a 4.0
+     */
+
+    public boolean estaAprobado(double notaFinal) {
+        return notaFinal >= 4.0;
+    }
 }

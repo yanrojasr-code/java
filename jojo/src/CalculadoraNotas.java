32981dc82d3182af5f82692b5951dba1b011325e
yanara <yan.rojasr@duocuc.cl>
2026-09-25 21:45:22 -0300

    espero

---
+++ jojo/src/CalculadoraNotas.java
@@ -0,0 +1,37 @@
+/**
+ * Gestiona operaciones basica relacionada con
+ * las notas de un estudiante
+ */
+public class CalculadoraNotas {
+
+    /**
+     *
+     * Calcula el promedio de tres evaluaciones.
+     *
+     * @param nota1 primera nota
+     * @param nota2 segunda nota
+     * @param nota3 tercera nota
+     * @return promedio de las tres notas
+     */
+
+
+    public double calcularPromedio( // Las tres evaluaciones tienen la misma ponderación.
+            double nota1,
+            double nota2,
+            double nota3
+    )
+
+
+    /**
+     * determina si el promedio permite aprobar
+     * @param promedio  ees el promedio final del estudiante
+     * @return true si el promedio es mayor o igual a 4.0
+     */
+
+    public boolean estaAprobado(double promedio >=4.0) {
+        return promedio >= 4.0;}
+    /*
+     * La escala utilizada considera 4.0
+     * como nota mínima de aprobación.
+     */
+}
